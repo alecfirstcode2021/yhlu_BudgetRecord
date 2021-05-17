9eb0583dc22b1637e57945c2b4c36419f2a51929
alecfirstcode2021 <78454908+alecfirstcode2021@users.noreply.github.com>
2021-05-17 11:01:31 -0500

    TR service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,7 @@
-const CACHE_NAME = "static-cache-v2";
+// const CACHE_NAME = "static-cache-v2";
+// const DATA_CACHE_NAME = "data-cache-v1";
+
+const CACHE_NAME = "static-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
