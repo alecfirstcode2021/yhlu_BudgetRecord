c8ccb71d8d056e73fedf9b22899aff4daa847a6a
alecfirstcode2021 <78454908+alecfirstcode2021@users.noreply.github.com>
2021-05-17 10:26:29 -0500

    troubleshoot server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static("public"));
 // });
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/onoffbudget',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -29,6 +29,16 @@ mongoose.connect(
     useFindAndModify: false,
   },
 );
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   },
+// );
 
 
 
